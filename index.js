64779536674c540edea0768ca74958ec8671910b
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-01-26 19:46:45 -0500

    Done.

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-    let rightNumbers = dodger.style.right.replace("px", "");
-    let right = parseInt(rightNumbers, 10);
-    
-    if (right > 0) {
-        dodger.style.right = `${right + 1}px`;
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
     }
 }
 
